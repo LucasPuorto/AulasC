6b870864a2661146f14abd6d7431809c1dd85a8f
Lucas Puorto <lucaspuorto@gmail.com>
2025-03-29 17:54:27 -0300

    aula nivel aventureiro

--- nivel_aventureiro.c
+++ nivel_aventureiro.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+    float nota1, nota2, nota3, media;
+
+    printf("----- PROGRAMA DE CALCULO DE MEDIA -----\n\n");
+
+    printf("Informe a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Informe a segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Informe a terceira nota: ");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("A media e de: %.2f\n", media);
+
+    return 0;
+}
